b4a528f9a0c4627461a4121e57d792c66d0591e5
ivanou <anjinsan132131@gmail.com>
2022-06-16 20:46:38 +0300

    удаляет неиспользуемый код

--- src/constans.js
+++ src/constans.js
@@ -1,26 +1,8 @@
-export const NUMBER_OF_EVENTS = 3;
-
-export const DESCRIPTION_TEXT = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  'Cras aliquet varius magna, non porta ligula feugiat eget.',
-  'Fusce tristique felis at fermentum pharetra.',
-  'Aliquam id orci ut lectus varius viverra.',
-  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
-  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
-  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
-  'Sed sed nisi sed augue convallis suscipit in sed felis.',
-  'Aliquam erat volutpat.',
-  'Nunc fermentum tortor ac porta dapibus.',
-  'In rutrum ac purus sit amet tempus.'
-];
-
 export const CITY_NAME = [
   'Chamonix',
   'Amsterdam',
   'Geneva'
 ];
-
-export const PHOTO_ADRESS = 'http://picsum.photos/300/200?r';
 
 export const POINT_TYPE = [
   'taxi',
@@ -66,9 +48,6 @@ export const EventMode = {
 };
 
 export const DisabledSortValue = ['event', 'offers'];
-
-export const MIN_PHOTO_VALUE = 1;
-export const MAX_PHOTO_VALUE = 10;
 
 export const UserAction = {
   UPDATE_POINT: 'UPDATE_POINT',
